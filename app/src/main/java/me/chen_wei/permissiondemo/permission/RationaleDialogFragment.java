4e2891059c98a45b651b878bf68124cd64ca9b5d
陈威 <hzchenwei6@corp.netease.com>
2016-12-29 15:25:01 +0800

    @bugfix 解决Dialog显示异常的Bug
    Signed-off-by: 陈威 <hzchenwei6@corp.netease.com>

--- app/src/main/java/me/chen_wei/permissiondemo/permission/RationaleDialogFragment.java
+++ app/src/main/java/me/chen_wei/permissiondemo/permission/RationaleDialogFragment.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Context;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.StringRes;
 
@@ -53,7 +54,8 @@ public class RationaleDialogFragment extends DialogFragment {
         mCallbacks = null;
     }
 
-    public Dialog onCreateDialog(Build savedInstanceState) {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
         setCancelable(false);
 
         mConfig = new RationaleDialogConfig(getArguments());
